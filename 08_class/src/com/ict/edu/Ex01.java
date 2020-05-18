18df0920c76cb014f0f2d733f2dfbba881559271
DrMJ-Chu <qjtmwkrnr@naver.com>
2020-05-18 18:26:19 +0900

    class3

---
+++ 08_class/src/com/ict/edu/Ex01.java
@@ -0,0 +1,35 @@
+package com.ict.edu;
+
+public class Ex01 {
+	// - 상속관계 : 자식클래스가 부모 클래스의 멤버필드, 멤버메소드를 객체 생성없이
+	//            마음대로 사용 할 수 있는 클래스간의 관계를 말한다.
+	//            자식클래스가 부모클래스와 관계를 맺는다.
+	//            자식클래스 extends 부모클래스
+	// - 자바에서는 다중 상속를 할 수 없다. 즉 부모클래스가 하나만 존재한다.
+	// - 모든 클래스는 Object라는 클래스를 상속받고 있다.
+	
+	public static void main(String[] args) {
+		
+		Ex01_sub test = new Ex01_sub();
+		// 자식 클래스를 호출
+		// > 부모클래스 생성 후 자식 클래스 생성
+		// > 부모.자식의 클래스 주소가 똑같음
+		// > 변수의 우선 순위 : 지역 변수 > 전역 변수 < 부모 클래스
+		System.out.println(test);
+		System.out.println("=============");
+		test.play();
+		
+		// 부모클래스의 메소드 사용가능
+		test.prn1();
+		
+		// static은 상속과 상관없이 사용 가능
+		System.out.println(Ex01_sup.car);
+		System.out.println(Ex01_sup.GENDER);
+		Ex01_sup.prn2();
+		
+		// private은 상속이어도 접근 안됨
+		// System.out.println(test.dog);
+		
+	}
+	
+}
---
+++ 08_class/src/com/ict/edu2/MP3Phone.java
@@ -0,0 +1,8 @@
+package com.ict.edu2;
+
+public class MP3Phone extends Phone{
+	public void sound() {
+		System.out.println("À½¾Ç µè´Â ±â´É");
+	}
+
+}
