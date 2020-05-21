c0abbf376f8c931f30942b079d0d454dc527f7eb
DrMJ-Chu <qjtmwkrnr@naver.com>
2020-05-21 18:14:28 +0900

    array

---
+++ 11_Collection/src/com/ict/edu/Ex04.java
@@ -0,0 +1,21 @@
+package com.ict.edu;
+
+public class Ex04 {
+	
+	private String name;
+	private int sum;
+	private double avg ;
+	private String hak ;
+	private int rank = 1;
+
+	// ÇÕ°è
+	public void s_sum(int kor, int eng, int math) {
+		sum = kor + eng + math ;
+		
+		s_avg();
+}
+	// Æò±Õ
+	public void s_avg() {
+		
+	}
+}
---
+++ 11_Collection/src/com/ict/edu/Ex04_main.java
@@ -0,0 +1,23 @@
+package com.ict.edu;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Ex04_main {
+	public static void main(String[] args) {
+		
+	
+	
+	// 이름, 국어, 영어, 수학 점수 입력하기
+	Scanner sc = new Scanner(System.in);
+	
+	Ex09[] arr = new Ex09[5];
+	
+	}
+		
+	
+	// 총점, 평균, 학점, 순위 구하기 및 정렬하기
+	// 출력하기
+	
+	}
+}
